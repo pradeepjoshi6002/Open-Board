3a5f782e50059cf069641521ca78d46f7167a282
Pradeep Joshi <pradeepjoshi6002@gmail.com>
2024-01-23 13:06:36 +0530

    added socket function for pencil only

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express();
+let port = 7000;
+
+app.use(express.static("public"));
+
+let server = app.listen(port, () => {
+  console.log("listening to port", port);
+});
+
+let io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("socket connection made");
+  //received data
+  socket.on("beginPath", (data) => {
+    //now transfer data to all connections
+    io.sockets.emit("beginPath", data);
+  });
+  socket.on("drawStroke", (data) => {
+    //now transfer data to all connections
+    io.sockets.emit("drawStroke", data);
+  });
+});
--- public/canvas.js
+++ public/canvas.js
@@ -6,7 +6,6 @@ let mouseDownFlag = false;
 
 let tool = canvas.getContext("2d");
 tool.lineWidth = 3;
-tool.strokeStyle = "blue";
 
 let tracker;
 const undoRedoTracker = [];
@@ -14,11 +13,15 @@ const undoRedoTracker = [];
 // mousedown->start new path, mousemove->path fill graphics
 canvas.addEventListener("mousedown", (e) => {
   mouseDownFlag = !mouseDownFlag;
-  beginPath({ x: e.clientX, y: e.clientY });
+  // beginPath({ x: e.clientX, y: e.clientY });
+  let data = { x: e.clientX, y: e.clienty };
+  socket.emit("beginPath", data);
 });
 canvas.addEventListener("mousemove", (e) => {
   if (mouseDownFlag) {
-    drawStroke({ x: e.clientX, y: e.clientY });
+    let data = { x: e.clientX, y: e.clientY };
+    socket.emit("drawStroke", data);
+    // drawStroke({ x: e.clientX, y: e.clientY });
   }
 });
 canvas.addEventListener("mouseup", (e) => {
@@ -36,3 +39,10 @@ function drawStroke(strokeObj) {
   tool.lineTo(strokeObj.x, strokeObj.y);
   tool.stroke();
 }
+
+socket.on("beginPath", (data) => {
+  beginPath(data);
+});
+socket.on("drawStroke", (data) => {
+  drawStroke(data);
+});
